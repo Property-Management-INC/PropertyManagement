a355eba54465fe58cb572d3392f77b029b0cf539
Graham2000 <wghiltonreads@gmail.com>
2023-02-11 18:32:57 -0500

    Add db conn

--- public/index.php
+++ public/index.php
@@ -2,20 +2,8 @@
 <?php include("includes/header.php"); ?>
 <?php include("includes/nav.php"); ?>
 
-
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
+  include("includes/connect.php");
 ?>
 
 <div id="imgContainer">
