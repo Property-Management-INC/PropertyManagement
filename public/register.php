573fe25c293a7dae0cf3eee3ff6f36f92c550e44
Graham2000 <wghiltonreads@gmail.com>
2023-02-14 15:47:31 -0500

    Hash pwd

--- public/register.php
+++ public/register.php
@@ -3,7 +3,7 @@
 <?php include("includes/nav.php"); ?>
 
 <div class="container middle pt-5 pb-5">
-  <form class="border p-3 w-75 rounded" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  <form class="border p-3 w-75 rounded" method="post" action="sql/insertUser.php">
     <div class="mb-3">
       <h1>Create account</h1>
     </div>
@@ -28,24 +28,6 @@
   </form>
 </div>
 
-<?php
-// Insert new user account
-echo print_r($_POST,true);
-// Get data
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $username = $_POST['username'];
-  $password = $_POST['password'];
-  $email = $_POST['email'];
 
-
-  //echo "</br>";
-  //echo $username;
-  //echo "</br>";
-  //echo $password;
-  //echo "</br>";
-  //echo $email;
-}
-
-?>
 
 <?php include("includes/footer.php"); ?>
---
+++ public/sql/insertUser.php
@@ -0,0 +1,37 @@
+<?php
+// Insert new user account
+include("../includes/connect.php");
+echo print_r($_POST,true);
+// Get data
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Get user input
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $email = $_POST['email'];
+
+  //echo "</br>";
+  //echo $username;
+  //echo "</br>";
+  //echo $password;
+  //echo "</br>";
+  //echo $email;
+
+  // Encrypt password
+  $password = password_hash($password, PASSWORD_DEFAULT);
+  echo $password;
+
+  // Insert into database
+  $sql = "INSERT INTO [USER].[USER] (POSTCODE, USERNAME_TXT, FIRST_NAME_TXT, LAST_NAME_TXT, STREET_ADDRESS_TXT, CITY, STATE, PASSWORD, EMAIL_ADDRESS_TXT, IS_ADMIN, IS_ACTIVE)
+  VALUES (12345, '$username', 'John', 'Doe', '2398 RANDOM RD', 'NY', 'NY', '$password', 'johnDoe@gmail.com', 0, 1)";
+
+  $stmt = sqlsrv_prepare($conn, $sql);
+
+  if (sqlsrv_execute($stmt) === false) {
+    echo "failed";
+  } else {
+    echo "new record created";
+  }
+  
+}
+
+?>
